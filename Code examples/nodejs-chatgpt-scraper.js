a87a2119473da2283403f55b4420dea626290e37
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2025-08-26 10:51:46 +0300

    Create nodejs-chatgpt-scraper.js

---
+++ Code examples/nodejs-chatgpt-scraper.js
@@ -0,0 +1,42 @@
+const https = require("https");
+
+const username = "USERNAME";
+const password = "PASSWORD";
+const body = {
+    source: "chatgpt",
+    prompt: "best supplements for better sleep",
+    parse: true,
+    search: true,
+    geo_location: "United States"
+};
+
+const options = {
+    hostname: "realtime.oxylabs.io",
+    path: "/v1/queries",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization:
+            "Basic " + Buffer.from(`${username}:${password}`).toString("base64"),
+    },
+};
+
+const request = https.request(options, (response) => {
+    let data = "";
+
+    response.on("data", (chunk) => {
+        data += chunk;
+    });
+
+    response.on("end", () => {
+        const responseData = JSON.parse(data);
+        console.log(JSON.stringify(responseData, null, 2));
+    });
+});
+
+request.on("error", (error) => {
+    console.error("Error:", error);
+});
+
+request.write(JSON.stringify(body));
+request.end();
