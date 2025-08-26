2dc9e968a1ced953988d35d2e7c2e167e57fc330
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2025-08-26 10:53:31 +0300

    Create php-chatgpt-scraper.php

---
+++ Code examples/php-chatgpt-scraper.php
@@ -0,0 +1,30 @@
+<?php
+
+$params = array(
+    'source' => 'chatgpt',
+    'prompt' => 'best supplements for better sleep',
+    'parse' => true,
+    'search' => true,
+    'geo_location' => "United States"
+);
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "USERNAME" . ":" . "PASSWORD");
+
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
