05145e08bf0a057a6dc7245e63e39898c6f5f275
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2026-08-26 22:12:48 +0300

    update curl code

--- Code examples/curl-chatgpt-scraper.sh
+++ Code examples/curl-chatgpt-scraper.sh
@@ -1,10 +1,10 @@
-curl 'https://realtime.oxylabs.io/v1/queries' \
+curl 'https://data.oxylabs.io/v1/queries' \
 --user 'USERNAME:PASSWORD' \
 -H 'Content-Type: application/json' \
 -d '{
         "source": "chatgpt",
         "prompt": "best supplements for better sleep",
         "parse": true,
-        "search": true,
-        "geo_location": "United States"
+        "geo_location": "United States",
+        "callback_url": "https://your-server.com/oxylabs-callback"
     }'
