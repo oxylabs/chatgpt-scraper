70d47a6a54a9dc3758b592617a6b355ccbe14b96
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2026-08-26 22:13:15 +0300

    update golang code
    Updated payload to include callback URL and changed the endpoint URL for queries.

--- Code examples/golang-chatgpt-scraper.go
+++ Code examples/golang-chatgpt-scraper.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
+"bytes"
+"encoding/json"
+"fmt"
+"io/ioutil"
+"net/http"
 )
 
 func main() {
-	const Username = "USERNAME"
-	const Password = "PASSWORD"
+const Username = "USERNAME"
+const Password = "PASSWORD"
 
-  	payload := map[string]interface{}{
-        	"source": "chatgpt",
-        	"prompt": "best supplements for better sleep",
-        	"parse":  true,
-        	"search": true,
-        	"geo_location": "United States"
-    	}
+  payload := map[string]interface{}{
+        "source": "chatgpt",
+        "prompt": "best supplements for better sleep",
+        "parse":  true,
+        "geo_location": "United States",
+        "callback_url": "https://your-server.com/oxylabs-callback",
+    }
 
-	jsonValue, _ := json.Marshal(payload)
+jsonValue, _ := json.Marshal(payload)
 
-	client := &http.Client{}
-	request, _ := http.NewRequest("POST",
-		"https://realtime.oxylabs.io/v1/queries",
-		bytes.NewBuffer(jsonValue),
-	)
+client := &http.Client{}
+request, _ := http.NewRequest("POST",
+"https://data.oxylabs.io/v1/queries",
+bytes.NewBuffer(jsonValue),
+)
 
-	request.SetBasicAuth(Username, Password)
-	response, _ := client.Do(request)
+request.SetBasicAuth(Username, Password)
+response, _ := client.Do(request)
 
-	responseText, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(responseText))
+responseText, _ := ioutil.ReadAll(response.Body)
+fmt.Println(string(responseText))
 }
